54804a804929cb269dd6f81e6d1aaf865fd6c65a
Claes Holmerson <claes@holmerson.net>
2020-10-01 21:48:17 +0200

    Doc update

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# This script cleans up the installed portable service from the host system
+
 sudo systemctl stop portablehomeassistant
 sudo systemctl disable portablehomeassistant
 sudo portablectl detach machines/homeassistant/ portablehomeassistant.service
